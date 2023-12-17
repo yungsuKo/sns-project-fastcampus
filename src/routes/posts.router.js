706d4737dfd0cb8d10cb89c2fdf82e98fbdae002
Tom Ko <yungsu2391@gmail.com>
2023-12-17 22:58:19 +0900

    post ui 생성

--- src/routes/posts.router.js
+++ src/routes/posts.router.js
@@ -1,9 +1,15 @@
 const express = require('express');
 const { checkAuthenticated } = require('../middlewares/auth');
 const postsRouter = express.Router();
+const Post = require('../models/posts.model');
+const Comment = require('../models/comments.model');
 
-postsRouter.get('/', checkAuthenticated, (req, res) => {
-    res.render('posts/index')
+postsRouter.get('/', checkAuthenticated, async(req, res) => {
+    const posts = await Post.find().populate('comments').sort({createdAt: -1})
+    res.render('posts/index', {
+        posts: posts,
+        currentUser: req.user
+    })
 })
 
 module.exports = postsRouter;
