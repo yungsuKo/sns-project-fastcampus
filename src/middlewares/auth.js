cb70ce7e8fdd71eb9e5e8f93a83e80b4b3f8e275
Tom Ko <yungsu2391@gmail.com>
2023-12-20 14:29:14 +0900

    post owner auth middleware

--- src/middlewares/auth.js
+++ src/middlewares/auth.js
@@ -1,3 +1,5 @@
+const Post = require('../models/posts.model');
+
 function checkAuthenticated(req, res, next) {
   if (req.isAuthenticated()) {
     return next();
@@ -12,4 +14,25 @@ function checkNotAuthenticated(req, res, next) {
   next();
 }
 
-module.exports = { checkAuthenticated, checkNotAuthenticated };
+async function checkPostOwnership(req, res, next) {
+  if(req.isAuthenticated()){
+    const post = await Post.findById(req.params.id);
+    if(!post){
+      req.flash('error', '포스트가 없거나 에러가 발생했습니다.');
+      res.redirect('back');
+    }else{
+      if(post.author.id.equals(req.user._id)){
+        req.post = post;
+        next();
+      }else{
+        req.flash('error', '권한이 없는 포스트 입니다.');
+        res.redirect('back');
+      }
+    }
+  }else{
+    req.flash('error', '로그인을 먼저 해주세요');
+    res.redirect('/login')
+  }
+}
+
+module.exports = { checkAuthenticated, checkNotAuthenticated, checkPostOwnership };
