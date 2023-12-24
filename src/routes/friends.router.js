2a3dc0069f2bb502fe4e0b1a80f2c7e16d2e2cc9
Tom Ko <yungsu2391@gmail.com>
2023-12-24 23:30:24 +0900

    friend 화면 생성

--- src/routes/friends.router.js
+++ src/routes/friends.router.js
@@ -1,4 +1,18 @@
 const express = require('express');
+const { checkAuthenticated } = require('../middlewares/auth');
+const User = require('../models/users.model');
 const router = express.Router();
+
+router.get('/', checkAuthenticated, async (req, res, next) => {
+    const users = await User.find();
+    if(!users){
+        req.flash('error', '유저가 없습니다.');
+        res.redirect('back');
+    }else{
+        res.render('friends/index', {
+            users
+        })
+    }
+})
 
 module.exports = router;
