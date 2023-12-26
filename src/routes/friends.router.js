0b50bab4363259f81b9beb4b16e2b90f484b9cc0
Tom Ko <yungsu2391@gmail.com>
2023-12-26 20:12:29 +0900

    친구 요청 수락/삭제 api

--- src/routes/friends.router.js
+++ src/routes/friends.router.js
@@ -53,4 +53,50 @@ router.put('/:id/remove-friend-request', checkAuthenticated, async (req, res, ne
     }
 })
 
+router.put('/:id/accept-friend-request', checkAuthenticated, async(req, res, next) => {
+    const requestUser = await User.findById(req.params.id);
+    if(!requestUser){
+        req.flash('error', '유저를 찾지 못했습니다.');
+        res.redirect('back');
+    }else {
+        const user = await User.findByIdAndUpdate(req.user._id, {
+            friendsRequests: req.user.friendsRequests.filter((userReq) => userReq === requestUser._id),
+            friends: req.user.friends.concat([requestUser._id]),
+        });
+        await User.findByIdAndUpdate(req.params.id, {
+            friends: req.user.friends.concat([user._id]),
+        });
+        console.log(user);
+        if(!user){
+            req.flash('error', '친구 추가에 실패했습니다.');
+            res.redirect('back');
+        }else{
+            req.flash('success', '친구 추가에 성공했습니다.');
+            res.redirect('back');
+        }
+    }
+})
+
+router.put('/:id/remove-friend', checkAuthenticated, async (req, res) => {
+    const removeUser = await User.findById(req.params.id);
+    if(!removeUser){
+        req.flash('error', '유저가 조회되지 않습니다.');
+        res.redirect('back');
+    }else{
+        const user = await User.findByIdAndUpdate(req.user._id, {
+            friends: req.user.friends.filter((friend) => friend === removeUser._id)
+        });
+        await User.findByIdAndUpdate(req.params.id, {
+            friends: removeUser.friends.filter((friend) => friend === user._id)
+        });
+        if(!user){
+            req.flash('error', '친구 삭제에 실패했습니다.');
+            res.redirect('back');
+        } else {
+            req.flash('success', '친구 삭제에 성공했습니다.');
+            res.redirect('back');
+        }
+    }
+})
+
 module.exports = router;
