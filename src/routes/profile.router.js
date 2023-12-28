f5840303b3595f7b009d29c3cea2a20942e6a62d
Tom Ko <yungsu2391@gmail.com>
2023-12-28 20:15:12 +0900

    profile get api

--- src/routes/profile.router.js
+++ src/routes/profile.router.js
@@ -1,4 +1,11 @@
 const express = require('express');
+const { checkAuthenticated } = require('../middlewares/auth');
 const router = express.Router();
+
+router.get('/', checkAuthenticated, async (req, res) => {
+    res.render('profile/index', {
+        user: req.user
+    })
+})
 
 module.exports = router;
